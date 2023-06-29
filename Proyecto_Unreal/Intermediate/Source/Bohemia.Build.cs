f70d98ce0334c4e2d1f46c8e85a8e6cc71e4e70b
Francisco260998 <Ciccotosto.mariano1468@gmail.com>
2023-06-28 22:17:27 -0300

    v_6

---
+++ Proyecto_Unreal/Intermediate/Source/Bohemia.Build.cs
@@ -0,0 +1,12 @@
+using UnrealBuildTool;
+
+public class Bohemia : ModuleRules
+{
+	public Bohemia(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PrivateDependencyModuleNames.Add("Core");
+		PrivateDependencyModuleNames.Add("Core");
+	}
+}
